fa2abdfc46ea5984c5ae504e0bd5a488d612f0ab
TRP <66782725+TRP0207@users.noreply.github.com>
2022-05-21 19:44:32 +0530

    Delete loggingSystem directory

--- loggingSystem/src/main/java/com/loggingSystem/service/UserDetailService.java
+++
@@ -1,7 +0,0 @@
-package com.loggingSystem.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailService {
-    public UserDetails LoadUserByUserName(String userName);
-}
